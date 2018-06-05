c31b9e62baa6c6a219bd5a5bd493033cb1cd2558
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-05 12:17:56 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,37 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+def get_characters
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+end
+
+def get_character_info(chars_array, char)
+  chars_array["results"].find { |char_hash| char_hash["name"].downcase == char.downcase }
+end
+
+def get_film_urls(character_info)
+  character_info["films"].map do |url|
+    string = RestClient.get(url)
+    JSON.parse(string)
+  end
+end
+
+def get_character_movies_from_api(character)
+  characters = get_characters
+  character_info = get_character_info(characters, character)
+  get_film_urls(character_info)
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |hash|
+    puts "================================"
+    puts "#{hash["title"]} (#{hash["release_date"].slice(0, 4)})"
+    puts "================================"
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
